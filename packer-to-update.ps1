1d06cb36bd8bdf0b60676f918fcfa94604db83fe
Ajish Antony <48723128+ajish-antony@users.noreply.github.com>
2021-09-20 00:55:00 +0530

    Add files via upload

---
+++ packer-to-update.ps1
@@ -0,0 +1,43 @@
+#######################################################
+            # Variable Declaration
+#######################################################
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    default = "Enter-Require-Instance-Type"
+}
+
+#######################################################
+            # Timestamp Variable
+#######################################################
+
+locals {
+  timestamp = regex_replace(timestamp(), "[- TZ:]", "")
+}
+
+source "amazon-ebs" "ec2-docker" {
+  ami_name                  = "Packer-AMI-${local.timestamp}"
+  ami_description           = "Amazon Linux 2 Imaged-AMI Created Via Packer"
+  instance_type             = "${var.instance_type}"
+  region                    = "${var.region}"
+  source_ami_filter   {
+    filters                 = {
+      name                  = "amzn2-ami-hvm-2.0.*.1-x86_64-ebs"
+      root-device-type      = "ebs"
+      virtualization-type   = "hvm"
+    }
+    most_recent             = true
+    owners                  = ["amazon"]
+  }
+  ssh_username              = "ec2-user"
+}
+build {
+  sources = ["source.amazon-ebs.ec2-docker"]
+
+provisioner "ansible" {
+      playbook_file = "./ansible.yml"
+  }
+}
